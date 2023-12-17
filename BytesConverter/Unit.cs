416647fc7ecb545bcf23b735d3aea345fa3c77c6
David Sanchez <14120980+tazlord@users.noreply.github.com>
2023-12-16 18:21:17 -0800

    Add documentation to Unit.cs

--- BytesConverter/Unit.cs
+++ BytesConverter/Unit.cs
@@ -2,16 +2,59 @@
 {
     public enum Unit
     {
+        /// <summary>
+        /// Represents the raw bytes, no conversion (default).
+        /// </summary>
         NONE,
+
+        /// <summary>
+        /// Represents bytes converted to kilobytes (KB).
+        /// </summary>
         KILOBYTE,
+
+        /// <summary>
+        /// Represents bytes converted to kibibytes (KiB).
+        /// </summary>
         KIBIBYTE,
+
+        /// <summary>
+        /// Represents bytes converted to megabytes (MB).
+        /// </summary>
         MEGABYTE,
+
+        /// <summary>
+        /// Represents bytes converted to mebibytes (MiB).
+        /// </summary>
         MEBIBYTE,
+
+        /// <summary>
+        /// Represents bytes converted to gigabytes (GB).
+        /// </summary>
         GIGABYTE,
+
+        /// <summary>
+        /// Represents bytes converted to gibibytes (GiB).
+        /// </summary>
         GIBIBYTE,
+
+        /// <summary>
+        /// Represents bytes converted to terabytes (TB).
+        /// </summary>
         TERABYTE,
+
+        /// <summary>
+        /// Represents bytes converted to tebibytes (TiB).
+        /// </summary>
         TEBIBYTE,
+
+        /// <summary>
+        /// Represents bytes converted to petabytes (PB).
+        /// </summary>
         PETABYTE,
+
+        /// <summary>
+        /// Represents bytes converted to pebibytes (PiB).
+        /// </summary>
         PEBIBYTE
     }
 }
