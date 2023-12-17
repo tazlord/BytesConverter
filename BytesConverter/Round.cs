1b5e5a222237cea261873a2bb62fb1fd231f5fd1
David Sanchez <14120980+tazlord@users.noreply.github.com>
2023-12-16 19:34:36 -0800

    Add copyright notices

--- BytesConverter/Round.cs
+++ BytesConverter/Round.cs
@@ -1,4 +1,22 @@
-﻿namespace BytesConverter
+﻿//  BytesConverter is a C# utility class for converting bytes to other
+//  data storage units.
+//
+//  Copyright (C)2023 Jose Sanchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace BytesConverter
 {
     /// <summary>
     /// Specifies rounding options for use in the <see cref="Converter"/> class.
